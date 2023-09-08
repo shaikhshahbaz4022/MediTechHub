8549485e84bb08cf7e1cd3161174a908ba1d219d
unknown <shaikhshahbaz4022@gmail.com>
2023-09-08 21:20:59 +0530

    Bug Fixed By MD_Saif

--- Backend/src/connection/config.ts
+++ Backend/src/connection/config.ts
@@ -1,4 +1,4 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
-export const connection = mongoose.connect(process.env.mongoURL || "");
+export const connection = mongoose.connect(process.env.mongoURl || "");
